9ad6eca6e86c671d3a7897b1700036176d37bedd
sswellington <wellingtons@id.uff.br>
2020-06-28 19:59:28 -0300

    bootstrap - page/assets
    feat: page/assets
    
    refactor:
    * biblioteca
    * dice
    * imc
    * ingresso
    * malacomalca

---
+++ page/biblioteca/pretensao.php
@@ -0,0 +1,16 @@
+<?php
+    $head = "src/include/head.html";
+    $css = "src/include/css.html";
+	$nav = "src/include/nav.html";
+	$footer = "src/include/footer.html";
+	$javascript = "src/include/javascript/nav.html";
+
+	$main = "src/html/pretensao.html";
+
+	include $head;
+	include $css;
+	include $nav;
+	include $main;
+	include $footer;
+	include $javascript;
+?>	
